cbace394fc0ef5b995d0fd662dcfb510753ba96b
ansh <ansh@uw.edu>
2019-09-01 15:14:46 -0400

    created JS object with roles

---
+++ ONUWW/roles/roles.js
@@ -0,0 +1,66 @@
+const roles = 
+[
+	{
+		'name': 'Werewolf',
+		'img': 'Werewolf.png',
+		'team': 'Werewolf',
+		'win-condition': 'Kill a Townie',
+		'power': 'Sees other Werewolves'
+	},
+
+	{
+		'name': 'Seer',
+		'img': 'Seer.png',
+		'team': 'Town',
+		'win-condition': 'Kill a Werewolf',
+		'power': 'Sees one player\'s card or two center cards'
+	},
+
+	{
+		'name': 'Doppelganger',
+		'img': 'Doppelganger.png',
+		'team': 'Same as Copied Card',
+		'win-condition': 'Same as Copied Card',
+		'power': 'Copies someone\'s card and gains their power'
+	},
+
+	{
+		'name': 'Robber',
+		'img': 'Robber.png',
+		'team': 'Town',
+		'win-condition': 'Kill a Werewolf',
+		'power': 'May steal another player\'s card and view it'
+	},
+
+	{
+		'name': 'Tanner',
+		'img': 'Tanner.png',
+		'team': 'Self',
+		'win-condition': 'Get Killed',
+		'power': 'None'
+	},
+
+	{
+		'name': 'Troublemaker',
+		'img': 'Troublemaker.png',
+		'team': 'Town',
+		'win-condition': 'Kill a Werewolf',
+		'power': 'May swap two other players\' cards'
+	},
+
+	{
+		'name': 'Mason',
+		'img': 'Mason.png',
+		'team': 'Town',
+		'win-condition': 'Kill a Werewolf',
+		'power': 'Sees other Masons'
+	},
+
+	{
+		'name': 'Seer',
+		'img': 'Seer.png',
+		'team': 'Town',
+		'win-condition': 'Kill a Werewolf',
+		'power': 'Sees own card'
+	}
+];
